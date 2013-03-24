9b55c2dd39f14edcafee292f0439ed5318f82a62
ntvf <ntvf@ukr.net>
2013-03-24 22:48:03 +0200

    Added Class for manipulating the porter, metod stubs

---
+++ src/main/java/ua/porter/model/PorterManipulator.java
@@ -0,0 +1,35 @@
+package ua.porter.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tema
+ * Date: 3/24/13
+ * Time: 10:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PorterManipulator implements Joystick {
+    @Override
+    public void down() {
+
+    }
+
+    @Override
+    public void up() {
+
+    }
+
+    @Override
+    public void left() {
+
+    }
+
+    @Override
+    public void right() {
+
+    }
+
+    @Override
+    public void act() {
+
+    }
+}
