9e347f13b051cec5d05b602f1f0da7b22e631f60
ntvf <ntvf@ukr.net>
2013-03-24 10:33:30 +0200

    Adding Readme

---
+++ src/test/java/ua/porter/model/MainLoopTest.java
@@ -0,0 +1,13 @@
+package ua.porter.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tema
+ * Date: 3/24/13
+ * Time: 10:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainLoopTest {
+
+
+}
